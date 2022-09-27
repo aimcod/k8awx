825d268a637d70d36598896b373a26b7540c2ef2
aimcod <andrei@aimcorp.co.uk>
2022-09-27 22:46:45 +0200

    added kubeDeploy.sh script for kubernetes installation

---
+++ kubeDeploy.sh
@@ -0,0 +1,46 @@
+sudo firewall-cmd --permanent --zone=trusted --add-port=6443/tcp --add-port=2379-2380/tcp
+sudo firewall-cmd --permanent --zone=trusted --add-port=10251/tcp
+sudo firewall-cmd --permanent --zone=trusted --add-port=10252/tcp
+sudo firewall-cmd --permanent --zone=trusted --add-port=10250/tcp
+sudo firewall-cmd --permanent --zone=trusted --add-port=30000-32767/tcp
+sudo firewall-cmd --permanent --zone=trusted --add-port=80/tcp
+sudo firewall-cmd --permanent --zone=trusted --add-port=443/tcp
+sudo firewall-cmd --permanent --zone=trusted --add-port=22/tcp
+sudo firewall-cmd --reload
+sudo firewall-cmd --list-all
+sudo modprobe overlay
+sudo modprobe br_netfilter
+sudo cat << EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+sudo cat << EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sudo sysctl --system
+sudo apt update
+sudo apt install containerd
+sudo mkdir  -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+sudo echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
+#apt-cache policy kubelet | head -n 20
+#VERSION=$(apt-cache policy kubelet | head -n 7 | tail -n 1 | cut -d' ' -f 6)
+#apt install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl containerd
+sudo systemctl enable --now containerd kubelet
+sudo kubeadm init --ignore-preflight-errors=all
+
+mkdir -p $HOME/.kube
+sudo cp -f /etc/kubernetes/admin.conf /home/andrei/.kube/config 
+sudo chown andrei:andrei /home/andrei/.kube/config
+
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+kubectl get nodes
+kubectl get pods -A
